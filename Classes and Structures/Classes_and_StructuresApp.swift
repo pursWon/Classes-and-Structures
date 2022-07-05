72a63378d7cdc56580071510532800f225cb9f0b
Wongi <hpurs95@gmail.com>
2022-07-05 13:12:23 +0900

    Initial Commit

---
+++ Classes and Structures/Classes_and_StructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Classes_and_StructuresApp.swift
+//  Classes and Structures
+//
+//  Created by MacBook Air on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct Classes_and_StructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
